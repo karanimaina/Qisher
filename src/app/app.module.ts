6440cc12ca373fa4ed32d69d71060327c3782020
karanimaina <karani.maina2010@gmail.com>
2023-01-30 17:41:03 +0300

    add the home template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './components/home/home.component';
+import { NavigationComponent } from './navigation/navigation.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AboutComponent
+    AboutComponent,
+    HomeComponent,
+    NavigationComponent
   ],
   imports: [
     BrowserModule,
