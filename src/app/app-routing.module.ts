8e65c71a8f23ddbee2b2703d4994402efed6855b
karanimaina <karani.maina2010@gmail.com>
2023-01-30 18:22:37 +0300

    enable blog routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import {HomeComponent} from "./components/home/home.component";
 import {AboutComponent} from "./about/about.component";
 import {NavigationComponent} from "./components/navigation/navigation.component";
+import {BlogComponent} from "./components/blog/blog.component";
 
 const routes: Routes = [
   {path:'',component:HomeComponent},
   {path:'home',component:HomeComponent},
   {path:'about',component:AboutComponent},
-  {path:'nav',component:NavigationComponent}
+  {path:'nav',component:NavigationComponent},
+  {path:'blog',component:BlogComponent}
 ];
 
 @NgModule({
