e783d97bc6797282140c6e7647dab00dd43a40f0
karanimaina <karani.maina2010@gmail.com>
2023-01-30 18:15:45 +0300

    add nav  component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HomeComponent} from "./components/home/home.component";
+import {AboutComponent} from "./about/about.component";
+import {NavigationComponent} from "./components/navigation/navigation.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:HomeComponent},
+  {path:'home',component:HomeComponent},
+  {path:'about',component:AboutComponent},
+  {path:'nav',component:NavigationComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
