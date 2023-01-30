6aea7c9a1b26b204e8886404475eb13a2d2e9067
karanimaina <karani.maina2010@gmail.com>
2023-01-30 18:47:15 +0300

    addd blog details

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {HomeComponent} from "./components/home/home.component";
-import {AboutComponent} from "./about/about.component";
+import {AboutComponent} from "./components/about/about.component";
 import {NavigationComponent} from "./components/navigation/navigation.component";
 import {BlogComponent} from "./components/blog/blog.component";
+import {BlogDetailsComponent} from "./components/blog-details/blog-details.component";
 
 const routes: Routes = [
   {path:'',component:HomeComponent},
   {path:'home',component:HomeComponent},
   {path:'about',component:AboutComponent},
   {path:'nav',component:NavigationComponent},
-  {path:'blog',component:BlogComponent}
+  {path:'blog',component:BlogComponent},
+  {path:'blog-details',component:BlogDetailsComponent},
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AboutComponent } from './about/about.component';
+import { AboutComponent } from './components/about/about.component';
 import { HomeComponent } from './components/home/home.component';
 import { NavigationComponent } from './components/navigation/navigation.component';
 import { BlogComponent } from './components/blog/blog.component';
+import { BlogDetailsComponent } from './components/blog-details/blog-details.component';
 
 @NgModule({
   declarations: [
@@ -15,6 +16,7 @@ import { BlogComponent } from './components/blog/blog.component';
     HomeComponent,
     NavigationComponent,
     BlogComponent,
+    BlogDetailsComponent,
 
   ],
   imports: [
